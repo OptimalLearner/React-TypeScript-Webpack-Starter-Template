568654b399fbb81695d168edf97c492f6aada792
Keval Dhanani <dhanani.kev01@gmail.com>
2023-03-19 20:49:20 +0530

    Added Babel Runtime Plugin and Copy Plugin

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
     semi: false,
     trailingComma: 'es5',
     singleQuote: true,
-    printWidth: 80,
+    printWidth: 150,
     tabWidth: 4,
     endOfLine: 'auto',
 }
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -28,7 +29,7 @@ module.exports = {
             {
                 test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
                 type: 'asset/inline',
-            }
+            },
         ],
     },
     output: {
@@ -38,6 +39,9 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, '..', './src/index.html'),
+        }),
+        new CopyPlugin({
+            patterns: [{ from: 'source', to: 'dest', noErrorOnMissing: true }],
         }),
     ],
 }
