0f15ff97270aeb6abb4e1514b3cf8aeb4e294e5e
Keval Dhanani <dhanani.kev01@gmail.com>
2023-03-19 15:04:22 +0530

    Webpack Config For Loading Images Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './styles.css'
+import IMAGE from './react.png'
 
 export const App = () => {
     return (
-        <h1>
-            React TypeScript Webpack Starter Template
-        </h1>
+        <>
+            <h1>
+                React TypeScript Webpack Starter Template
+            </h1>
+            <img src={IMAGE} alt="React Logo" width="300" height="170" />
+        </>
     )
 }
