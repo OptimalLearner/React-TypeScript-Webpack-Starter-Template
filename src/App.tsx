43fa017e1120b59ee0f010d66069ad4c830efbb3
Keval Dhanani <dhanani.kev01@gmail.com>
2023-03-19 15:52:17 +0530

    Webpack Config For Loading SVGs Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './styles.css'
 import IMAGE from './react.png'
+import LOGO from './logo.svg'
 
 export const App = () => {
     return (
@@ -8,6 +9,7 @@ export const App = () => {
                 React TypeScript Webpack Starter Template
             </h1>
             <img src={IMAGE} alt="React Logo" width="300" height="170" />
+            <img src={LOGO} alt="React Logo" width="170" height="170" />
         </>
     )
 }
